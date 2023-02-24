e37fd03ce5d54bb3be6c1df8312308ba324bc4e5
lidiahasani <lidiahasani98@gmail.com>
2023-02-24 01:48:38 +0100

    Implement CRUD operations for Projekt

--- src/main/java/com/lidia/organization/repositories/PunonjesRepository.java
+++ src/main/java/com/lidia/organization/repositories/PunonjesRepository.java
@@ -1,9 +1,16 @@
 package com.lidia.organization.repositories;
 
 import com.lidia.organization.model.Punonjes;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PunonjesRepository extends JpaRepository<Punonjes, Integer> {
 
+    Punonjes findByEmer(String emer);
+
+    Punonjes findById(int id);
+
+    @Transactional
+    void deletePunonjesById(int id);
 
 }
